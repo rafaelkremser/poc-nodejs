1b73f62e36e5ade07fe5121600a8986f9e0330fc
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-25 13:28:37 -0300

    feat: create search params in get users

--- src/server.js
+++ src/server.js
@@ -2,22 +2,24 @@ import http from 'node:http';
 
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
+import { extractQueryParams } from './utils/extract-query-prams.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
     await json(req, res);
 
-    const route = routes.find(route => {
+    const route = routes.find((route) => {
         return route.method === method && route.path.test(url);
     });
 
     if (route) {
         const routeParams = req.url.match(route.path);
 
-        req.params = { ...routeParams.groups };
+        const { query, ...params } = routeParams.groups;
 
-        console.log(req.params);
+        req.params = {};
+        req.query = query ? extractQueryParams(query) : {};
 
         return route.handler(req, res);
     }
