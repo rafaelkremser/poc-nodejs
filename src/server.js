b103b5e15021f90ad7397be14fe16591f83d8482
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-25 12:24:50 -0300

    feat: add new delete route

--- src/server.js
+++ src/server.js
@@ -1,33 +1,25 @@
 import http from 'node:http';
-import { json } from './middlewares/json.js';
-import { randomUUID } from 'node:crypto';
-import { Database } from './database.js';
 
-const database = new Database();
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
     await json(req, res);
 
-    if (method === 'GET' && url === '/users') {
-        const users = database.select('users');
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url);
+    });
 
-        return res.end(JSON.stringify(users));
-    }
+    if (route) {
+        const routeParams = req.url.match(route.path);
 
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body;
+        req.params = { ...routeParams.groups };
 
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-        };
+        console.log(req.params);
 
-        database.insert('users', user);
-
-        return res.writeHead(201).end();
+        return route.handler(req, res);
     }
 
     return res.writeHead(404).end();
